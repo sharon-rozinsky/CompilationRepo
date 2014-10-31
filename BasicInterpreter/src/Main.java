c0fa0749d9958c4d652bd6bb01899babcdd4e374
tomsivan <tomsivanx@gmail.com>
2014-10-31 18:10:53 +0200

    dgfhdfgh

--- BasicInterpreter/src/Main.java
+++ BasicInterpreter/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Guy");
 		System.out.println("Sharon1");
 		System.out.println("Tom");
+		System.out.println("tom");
 		System.out.println("Fuck'in Testing");
 		
 		
