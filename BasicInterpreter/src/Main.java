30f3041ed2015351852bc652d84100a5893e8c7b
guyreuven <guyreuven88@gmail.com>
2014-11-03 21:15:13 +0200

    Start to implement main. added sample .txt program.

--- BasicInterpreter/src/Main.java
+++ BasicInterpreter/src/Main.java
@@ -1,20 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// first entry to the code
-		System.out.println("Guy");
-		System.out.println("Sharon1");
-		System.out.println("Tom");
-		System.out.println("tom");
-		System.out.println("Fuck'in Testing");
+
+		BufferedReader inProgramTxtStream = null;
+		List<String> programTxt = new ArrayList<String>();		
+		//Read Program text file.
+		try {   
+			inProgramTxtStream = new BufferedReader(new FileReader(args[0]));
+			String str;
+			while ((str = inProgramTxtStream.readLine()) != null) {
+				programTxt.add(str);
+			}
+
+			inProgramTxtStream.close();
+
+			for(int i=0;i < programTxt.size();i++)
+			{
+				System.out.println(programTxt.get(i));
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		//Create Parser object and start Lexicographic analysis.
 		
-		System.out.println("Another change");
-		
-		int x = 4;
-		int y = 5;
+		Parser parser = new Parser(programTxt);
+		if(parser.LexicalAnalysis())
+		{ //Passed compilation continue to execution.
+			
+		}
+		else //Compilation errors print errors from logger.
+		{
+			
+		}
 	}
 
 }
